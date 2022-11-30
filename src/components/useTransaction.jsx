cfb4c4c6eff918ad6e65953220001713ed43ff1c
hafsa <112987785+hafsaelbekkali@users.noreply.github.com>
2022-11-30 16:34:23 +0100

    Update useTransaction.jsx

--- src/components/useTransaction.jsx
+++ src/components/useTransaction.jsx
@@ -1,87 +1,16 @@
-// import { useDispatch, useSelector } from 'react-redux'
-// // import { ExpenseActions } from './expense-slice'
-// import {
-//   incomeCategories,
-//   expenseCategories,
-//   resetCategories,
-// } from './Categories'
-
-// const useTransaction = (title) => {
-//   const dispatch = useDispatch()
-//   const transactions = useSelector((state) => state.expenses.transactions)
-//   resetCategories()
-
-//   //Transaction Filteration of a particular type (Income, Expense)
-//   const rightTransaction = transactions.filter(
-//     (transaction) => transaction.type === title
-//   )
-
-//   //Total amount of all Transactions
-//   const totalAmount = rightTransaction.reduce(
-//     (acc, transaction) => acc + transaction.amount,
-//     0
-//   )
-
-//   //categories of particular type
-//   const catogaries = title === 'Income' ? incomeCategories : expenseCategories
-
-//   //   console.log(rightTransaction, totalAmount, catogaries)
-//   // Amount of each category added
-//   rightTransaction.forEach((transaction) => {
-//     catogaries.forEach((category) => {
-//       if (transaction.category === category.type) {
-//         category.amount += transaction.amount
-//       }
-//     })
-//   })
-
-//   // Filter Categories with amount greater than 0
-//   const filteredCategories = catogaries.filter(
-//     (category) => category.amount > 0
-//   )
-//   //create chart data
-//   const chartData = {
-//     labels: filteredCategories.map((category) => category.type),
-//     datasets: [
-//       {
-//         data: filteredCategories.map((category) => category.amount),
-//         backgroundColor: filteredCategories.map((category) => category.color),
-//       },
-//     ],
-//     options: {
-//       responsive: true,
-//     },
-//   }
-//   return {
-//     chartData,
-//     totalAmount,
-//     filteredCategories,
-//   }
-// }
-
-// export default useTransaction
-
-
 import { useDispatch, useSelector } from 'react-redux'
 import { ExpenseActions } from './expense-slice'
-import {
-  incomeCategories,
-  expenseCategories,
-  resetCategories,
-} from './Categories'
+import {incomeCategories,expenseCategories,resetCategories} from './Categories'
 
 const useTransaction = (title) => {
   const dispatch = useDispatch()
   const transactions = useSelector((state) => state.expenses.transactions)
   resetCategories()
 
-  //Transaction Filteration of a particular type (Income, Expense)
   const rightTransaction = transactions.filter(
     (transaction) => transaction.type === title
   )
-  //   console.log(transactions, 'transactions')
 
-  //Total amount of all Transactions
   const totalAmount = rightTransaction.reduce(
     (acc, transaction) => acc + transaction.amount,
     0
@@ -90,8 +19,6 @@ const useTransaction = (title) => {
   //categories of particular type
   const catogaries = title === 'Income' ? incomeCategories : expenseCategories
 
-  //   console.log(rightTransaction, totalAmount, catogaries)
-  // Amount of each category added
   rightTransaction.forEach((transaction) => {
     catogaries.forEach((category) => {
       if (transaction.category === category.type) {
@@ -100,7 +27,6 @@ const useTransaction = (title) => {
     })
   })
 
-  // Filter Categories with amount greater than 0
   const filteredCategories = catogaries.filter(
     (category) => category.amount > 0
   )
